1e457a730ca76db9f7da14a62b90de12bcb5cbd9
poweg9141 <poweg9141@HRH-IC0027342.wrdsb.ca>
2017-02-17 10:05:54 -0500

    Initial Commit

--- src/Assignment2/A2Q3.java
+++ src/Assignment2/A2Q3.java
@@ -1,8 +1,10 @@
+package Assignment2;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Assignment2;
+
 
 import becker.robots.City;
 import becker.robots.Direction;
