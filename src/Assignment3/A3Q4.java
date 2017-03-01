b794f00f1e6fbdb5b926db7ed06684c2ea0af1ac
poweg9141 <poweg9141@HRH-IC0027342.wrdsb.ca>
2017-03-01 09:44:17 -0500

    Nothing Changed :0

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -27,7 +27,7 @@ public class A3Q4 {
     public static void main(String[] args) {
 
         City kw = new City();
-        RobotSE bob = new RobotSE(kw, 1, 1, Direction.EAST);
+        RobotSE bob = new RobotSE(kw, 6, 1, Direction.EAST);
 
         //SPAWNS THE WALLS FOR THE BOX
         //spawns the top wall
@@ -80,23 +80,37 @@ public class A3Q4 {
         }
 
         //BOB ESCAPES THE BOX
-
+        
+        //while statement to ensure if the break is directly in front of him he dosent go on forever
+        while(bob.frontIsClear() && !(bob.getStreet() > height) && !(bob.getAvenue() > width))
+            bob.move();
+        bob.turnRight();
+        
+        //escape label
         escape:
+        //while the front is clear he turns left
         while (true) {
             while (bob.frontIsClear()) {
                 bob.turnLeft();
+                //if the front is clear he escaped
                 if (bob.frontIsClear()) {
                     break escape;
                 }
+                //if its noot he turns right and moves
                 bob.turnRight();
                 bob.move();
             }
+            //when the front is not clear (he hit a corner)
+            //bob turns left
             bob.turnLeft();
+            //if the front is clear he escaped
             if (bob.frontIsClear()) {
                 break escape;
             }
+            //if its not he turns around
             bob.turnAround();
         }
+        //if he escaped he moves once to put him outside the box
         bob.move();
     }
 
